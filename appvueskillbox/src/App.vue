9a2f7cdb46f5fcbd1ad471f0b9172ae2cd7eb380
timmy010 <timsh010@gmail.com>
2021-04-10 18:08:26 +0500

    task 4.3 done

--- appvueskillbox/src/App.vue
+++ appvueskillbox/src/App.vue
@@ -1,68 +1,34 @@
 <template>
 <section class="catalog">
 
-  <productList :products="products"></productList>
+  <productList :products="products"/>
+  <basePagination v-model="page" :count="countProducts" :per-page="productsPerPage"/>
 
-  <ul class="catalog__pagination pagination">
-    <li class="pagination__item">
-      <a class="pagination__link pagination__link--arrow pagination__link--disabled" aria-label="Предыдущая страница">
-        <svg width="8" height="14" fill="currentColor">
-          <use xlink:href="#icon-arrow-left"></use>
-        </svg>
-      </a>
-    </li>
-    <li class="pagination__item">
-      <a class="pagination__link pagination__link--current">
-        1
-      </a>
-    </li>
-    <li class="pagination__item">
-      <a class="pagination__link" href="#">
-        2
-      </a>
-    </li>
-    <li class="pagination__item">
-      <a class="pagination__link" href="#">
-        3
-      </a>
-    </li>
-    <li class="pagination__item">
-      <a class="pagination__link" href="#">
-        4
-      </a>
-    </li>
-    <li class="pagination__item">
-      <a class="pagination__link" href="#">
-        ...
-      </a>
-    </li>
-    <li class="pagination__item">
-      <a class="pagination__link" href="#">
-        10
-      </a>
-    </li>
-    <li class="pagination__item">
-      <a class="pagination__link pagination__link--arrow" href="#" aria-label="Следующая страница">
-        <svg width="8" height="14" fill="currentColor">
-          <use xlink:href="#icon-arrow-right"></use>
-        </svg>
-      </a>
-    </li>
-  </ul>
 </section>
 </template>
 
 <script>
 import productList from './components/productList.vue';
+import basePagination from './components/basePagination.vue';
 import products from './data/products';
 
 export default {
-  components: { productList },
+  components: { productList, basePagination },
   name: 'App',
   data() {
     return {
-      products,
+      page: 1,
+      productsPerPage: 3,
     };
+  },
+  computed: {
+    products() {
+      const offset = (this.page - 1) * this.productsPerPage;
+      return products.slice(offset, offset + this.productsPerPage);
+    },
+    countProducts() {
+      return products.length;
+    },
   },
 };
 </script>
