f8e54b72ed44b3e93212baf4ea3a50c116402c73
timmy010 <timsh010@gmail.com>
2021-04-14 16:09:46 +0500

    baseColors in productFilter done

---
+++ appvueskillbox/src/components/baseColors.vue
@@ -0,0 +1,42 @@
+<template>
+  <ul class="colors">
+    <li
+      class="colors__item"
+      v-for="color in colors"
+      :key="color.id"
+    >
+      <label class="colors__label">
+        <input
+          class="colors__radio sr-only"
+          type="radio" name="color"
+          :value="color.id"
+          v-model.number="currentColorsId"
+        >
+        <span
+          class="colors__value"
+          :style="{ backgroundColor: color.value }"
+        >
+        </span>
+      </label>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentColorsId: 0,
+    };
+  },
+  props: ['colors', 'parentColorsId'],
+  watch: {
+    currentColorsId(value) {
+      this.$emit('update:parentColorsId', value);
+    },
+    parentColorsId(value) {
+      this.currentColorsId = value;
+    },
+  },
+};
+</script>
