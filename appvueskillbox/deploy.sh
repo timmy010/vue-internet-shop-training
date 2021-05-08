7a1a37493678fe4ba835210dc09bbfa4a8d6f418
timmy010 <timsh010@gmail.com>
2021-05-08 11:52:11 +0500

    deploy

---
+++ appvueskillbox/deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# остановить публикацию при ошибках
+set -e
+
+# сборка приложения
+npm run build
+
+# переход в каталог сборки
+cd dist
+
+#инициализация репозитория и загрузка кода в GitHub
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f https://github.com/timmy010/vue-im-build.gi master:gh-pages
+
+cd -
